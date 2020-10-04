2ad6c0cc86fdeb46abe3763fd4e92f68eb479ab8
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2020-10-04 05:41:12 -0400

    Preenchimento do meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Blog do Rodolfo do Nascimento Azevedo com artigos, tutorias e muito mais sobre o mundo da programação' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
